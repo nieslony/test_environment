cb4a5755d0ca495ff08830d917a3e76632183233
Claas Nieslony <bb@nieslony.at>
2022-09-16 16:54:34 +0200

    Make output more readable

--- Windows-Server-Core/make-vagrant-box.sh
+++ Windows-Server-Core/make-vagrant-box.sh
@@ -4,21 +4,37 @@ VM_NAME="win2022-tmpl"
 BOX_NAME="win2022"
 POOL_NAME="default"
 
-POOL_PATH=$( virsh pool-dumpxml --pool $POOL_NAME | xmllint --xpath 'string(/pool/target/path)' - )
-IMAGE=$POOL_PATH/$VM_NAME.qcow2
+function log {
+    cols=$( tput cols )
+    padding=$( eval printf -- '-%.0s' {1..${cols}} )
+    msg="--- $(date) --- $@ $padding"
+    eval echo ${msg:0:${cols}}
+}
+
+log Check Image
+IMAGE="$( virsh vol-path --pool default --vol $VM_NAME.qcow2 )"
 if [ ! -e "$IMAGE" ]; then
-    echo Image $IMAGE not found.
+    echo "Image $IMAGE does not exit"
     exit 1
 fi
 
+if [ ! -r "$IMAGE" ]; then
+    echo "Image $IMAGE not readable, downloading to /var/tmp"
+    IMAGE="/var/tmp/$VM_NAME.qcow2"
+    virsh vol-download --pool default --vol $VM_NAME.qcow2 --file $IMAGE --sparse || exit 1
+else
+    echo "Image $IMAGE exists and is readable"
+fi
+
 VIRTUAL_SIZE=$( qemu-img info $IMAGE | awk '/virtual size/ { print $3; }' )
+echo Virtual image size: ${VIRTUAL_SIZE}G
 
 TMP_DIR=$( mktemp --directory --tmpdir=/var/tmp )
 VAGRANT_FILE=$TMP_DIR/Vagrantfile
 METADATA_FILE=$TMP_DIR/metadata.json
 BOX_FILE=$TMP_DIR/$BOX_NAME.box
 
-echo Creating $METADATA_FILE
+log Creating $METADATA_FILE
 cat <<EOF > $METADATA_FILE
 {
     "provider": "libvirt",
@@ -27,7 +43,7 @@ cat <<EOF > $METADATA_FILE
 }
 EOF
 
-echo Creating $VAGRANT_FILE
+log Creating $VAGRANT_FILE
 cat <<EOF > $VAGRANT_FILE
 Vagrant.configure("2") do |config|
     config.vm.communicator = "winrm"
@@ -62,17 +78,7 @@ Vagrant.configure("2") do |config|
 end
 EOF
 
-echo Creating $BOX_FILE
-if [ -r "$IMAGE_" ]; then
-    echo file $IMAGE is not readable, exporting to temp file
-
-    TMP_IMAGE=$( mktemp /var/tmp/$VM_NAME.qcow2.XXXXXX)
-    virsh vol-download --pool default $VM_NAME.qcow2 /dev/stdout \
-        > $TMP_IMAGE
-#    \
-#        | dd conv=sparse status=progress \
-    IMAGE=$TMP_IMAGE
-fi
+log Creating $BOX_FILE
 tar \
     --transform 's/.*qcow2/box.img/' \
     --transform 's/.*\///' \
@@ -82,12 +88,10 @@ tar \
     $TMP_DIR/metadata.json $TMP_DIR/Vagrantfile $IMAGE \
     | pigz > $BOX_FILE || exit 1
 
-if [ -n "TMP_IMAGE" ]; then
-    rm -v $TMP_IMAGE
-fi
-
-echo Removing existing box $BOX_NAME
+log Removing existing box $BOX_NAME
 vagrant box remove $BOX_NAME
 
-echo Creating new box $BOX_NAME
+log Creating new box $BOX_NAME
 vagrant box add --name $BOX_NAME $BOX_FILE || exit 1
+
+log Done.
