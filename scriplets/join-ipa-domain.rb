7387abca7c883b6b101306410df6a34c7ab8154d
Claas Nieslony <bb@nieslony.at>
2023-06-10 16:41:00 +0200

    Update scriptlets

--- scriplets/join-ipa-domain.rb
+++
@@ -1,6 +0,0 @@
-dirname = File.dirname(__FILE__)
-
-config.vm.provision "Join IPA domain",
-        type: "ansible",
-        playbook: "#{dirname}/../ansible/join-ipa-domain.yml",
-        config_file: "#{dirname}/../ansible/ansible.cfg"
--- scriplets/role-webserver.rb
+++
@@ -1,6 +0,0 @@
-dirname = File.dirname(__FILE__)
-
-config.vm.provision "Install role webserver",
-        type: "ansible",
-        playbook: "#{dirname}/../ansible/webserver-roles.yml",
-        config_file: "#{dirname}/../ansible/ansible.cfg"
